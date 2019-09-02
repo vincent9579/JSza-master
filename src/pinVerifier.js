45764ed4ec7daf56d1bbac3ddbc4025d991c26f6
vincent9579 <vincent9579@gmail.com>
2019-09-02 19:04:48 +0800

    make it better

--- src/pinVerifier.js
+++ src/pinVerifier.js
@@ -1,4 +1,3 @@
-const utf8 = require('utf8');
 const RSA = require('node-bignumber');
 
 class PinVerifier {
@@ -11,10 +10,6 @@ class PinVerifier {
     const rsa = new RSA.Key();
     const chr = String.fromCharCode;
     const sessionKey = json.sessionKey;
-    const message =
-      utf8.encode(chr(sessionKey.length) +
-      sessionKey + chr(this.id.length) +
-      this.id + chr(this.password.length) + this.password);
     rsa.setPublic(json.nvalue, json.evalue);
     const credentials = rsa.encrypt(message).toString('hex');
     const keyname = json.keynm;
